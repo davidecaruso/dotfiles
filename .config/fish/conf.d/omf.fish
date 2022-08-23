dc536748d1ed2e456ac2dc05a5774ad4ca10faef
Davide Caruso <davide.caruso93@gmail.com>
2022-08-23 09:18:37 +0200

    Update omf.fish

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -1,17 +1,23 @@
-# Path to Oh My Fish install.
+# Path to Oh My Fish
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# Load Oh My Fish configuration
+source "$OMF_PATH/init.fish"
 
-# Add the go bin path to be able to execute our programs
-set -x GOPATH $HOME/go
+# Paths
+set -x GOPATH "$HOME/go"
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
+
+set -x DENO_PATH "$HOME/.deno/bin"
+set -x PATH $DENO_PATH:$HOME/.local/bin/:$PATH
 
 # Show default branch name
 set -g theme_display_git_default_branch yes
 
 # Remove greetings
 set fish_greeting
+
+# Enable kubectl autocompletion
+kubectl completion fish | source
